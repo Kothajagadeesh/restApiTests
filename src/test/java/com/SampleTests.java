49547d31c75a9f7a19e76bbc1c621ad766da2316
Jagadeesh Kotha <Jkotha@childrensplace.com>
2019-09-26 14:44:03 +0530

    new tests

--- src/test/java/com/SampleTests.java
+++ src/test/java/com/SampleTests.java
@@ -1,9 +1,11 @@
 package com;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -31,11 +33,7 @@ public class SampleTests {
     @Test
     public void sampleWithBodyAsFile() {
         String apiUrl = "https://reqres.in/api/users";
-        String jsonFile = "{\n" +
-                "  \"name\": \"morpheus\",\n" +
-                "  \"job\": \"leader\"\n" +
-                "}";
-        File file = new File(System.getProperty("user.dir")+"/src/main/resources/data.json");
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources/data.json");
         response = given().
                 headers("Content-Type", "application/json").
                 body(file).when().post(apiUrl);
@@ -54,5 +52,38 @@ public class SampleTests {
                 body(jsonFile).when().post(apiUrl);
         System.out.println(response.getStatusCode());
         System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public void sampleWithJsonPath() {
+        String apiUrl = "https://reqres.in/api/users";
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources/test.json");
+        JsonPath jsonPath = new JsonPath(file);
+        List<String> s = jsonPath.get("store.book.author");
+        String firstAuthor = jsonPath.get("store.book[0].author");
+        System.out.println(s);
+        System.out.println(firstAuthor);
+    }
+
+    @Test
+    public void sampleJsonPathWithResponse() {
+        String apiUrl = "https://reqres.in/api/users";
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources/data.json");
+        response = given().
+                headers("Content-Type", "application/json").
+                body(file).when().post(apiUrl);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody().asString());
+        JsonPath jsonPath = new JsonPath(response.getBody().asInputStream());
+        System.out.println(jsonPath.getString("name"));
+    }
+
+    @Test
+    public void sampleWithCookies() {
+        String apiUrl = "https://reqres.in/api/users";
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources/data.json");
+        response = given().
+                headers("Content-Type", "application/json").
+                body(file).when().post(apiUrl);
     }
 }
